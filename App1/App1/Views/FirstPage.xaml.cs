eb798dda10f112b0896a98d34130154e8dafe89d
Martin Blomberg <martin.blomberg@outlook.com>
2020-11-05 14:31:36 +0100

    TestProject Added

---
+++ App1/App1/Views/FirstPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using App1.Services;
+using App1.ViewModels;
+using Autofac;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FirstPage : ContentPage
+    {
+        public FirstPage()
+        {
+            InitializeComponent();
+            BindingContext = AppContainer.Container.Resolve<FirstPageViewModel>();
+        }
+    }
+}
